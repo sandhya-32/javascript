1186cadccd10c10d98acd305cdc655ab4408a4b9
sandhya-32 <61542370+sandhya-32@users.noreply.github.com>
2020-04-23 08:20:38 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+let welcome = 'welcome';
+let price = 99.99;
+price = price +1;
+price -= 2;
+let discounted = false;
+const amount = Number.parseInt(45678);
+let money = 56;
+money = money.toString();
+price= price.length;
+
+showMessage( price + amount);
+
+console.log("Any message");
